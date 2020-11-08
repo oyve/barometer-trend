8d47f59049905c2795b9149a9b8f99e2abbe6163
Oyvind Hansen <oyvind.hansen@outlook.com>
2020-11-08 19:50:57 -0400

    add three hour choice test

--- test/index.test.js
+++ test/index.test.js
@@ -154,6 +154,29 @@ describe("Integration Tests", function () {
     });
 
     describe("Function Tests - Trend chooser", function () {
+        it("it should be RISING.STEADY and pick the THREE HOUR prognose", function () {
+            //arrange     
+            barometer.clear();
+            //THREE HOURS: this should result in FALLING.STEADY
+            barometer.addPressure(barometer.minutesFromNow(-180), 101500);
+            barometer.addPressure(barometer.minutesFromNow(-160), 101500 + 5);
+
+            //ONE HOUR: this should result in FALLING.SLOWLY
+            barometer.addPressure(barometer.minutesFromNow(-50), 101500 + 6);
+            barometer.addPressure(barometer.minutesFromNow(-40), 101500 + 7);
+
+            //HALF HOUR: this should result in FALLING.CHANGING
+            barometer.addPressure(barometer.minutesFromNow(-20), 101500 + 8);
+            barometer.addPressure(barometer.minutesFromNow(0), 101500 + 9);
+
+            //act
+            var actual = barometer.getTrend();
+
+            //assert
+            assert.strictEqual(actual.tendency, "RISING");
+            assert.strictEqual(actual.trend, "STEADY");
+        });
+
         it("it should be RISING.RAPIDLY and pick the ONE HOUR prognose", function () {
             //arrange     
             barometer.clear();
