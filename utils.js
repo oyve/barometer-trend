3134f13aa4f3b5eb999f4ef8732381234d4cdbf4
Oyvind Hansen <oyvind.hansen@outlook.com>
2021-05-18 10:04:31 -0400

    pressure closest to

--- utils.js
+++ utils.js
@@ -25,16 +25,42 @@ function adjustPressureToSeaLevel(pressure, height, temperature = toKelvinFromCe
 
 /**
  * 
- * @param {*} pressures The pressures to filter
- * @param {*} minutes Since X minutes
+ * @param {Array<Object>} pressures The pressures to filter
+ * @param {number} minutes Since X minutes
  * @returns {Array<Object>} Subset of pressures
  */
 function getPressuresSince(pressures, minutes) {
+
 	let earlier = minutesFromNow(-Math.abs(minutes));
+
 	let subPressures = pressures.filter((p) => {
 		return p.datetime.getTime() >= earlier.getTime();
 	});
 	return subPressures;
+}
+
+function isNullOrUndefined(value) {
+	return (value === null || value === undefined);
+}
+
+/**
+ * 
+ * @param {Array<Object>} pressures Array of pressurs 
+ * @param {Date} datetime 
+ * @returns The pressure closest to the given datetime, null if not found
+ */
+function getPressureClosestTo(pressures, datetime) {
+	let previous = [...pressures].reverse().find((p) => p.datetime.getTime() <= datetime.getTime());
+	let next = pressures.find((p) => p.datetime.getTime() >= datetime.getTime());
+
+	if(isNullOrUndefined(next) && isNullOrUndefined(previous)) return null;
+	if(isNullOrUndefined(next)) return previous;
+	if(isNullOrUndefined(previous)) return next;
+
+	let diffNext = Math.abs(next.datetime.getTime() - datetime.getTime());
+	let diffPrevious =  Math.abs(previous.datetime.getTime() - datetime.getTime());
+
+	return (diffNext < diffPrevious) ? next : previous;
 }
 
 function isSummer(isNorthernHemisphere = true) {
@@ -44,6 +70,11 @@ function isSummer(isNorthernHemisphere = true) {
 	return isNorthernHemisphere ? summer : !summer;
 }
 
+/**
+ * 
+ * @param {number} celcius Celcius degrees
+ * @returns Kelvin degrees
+ */
 function toKelvinFromCelcius(celcius) {
 	return celcius + 273.15;
 }
@@ -54,6 +85,7 @@ module.exports = {
 	isSummer,
 	adjustPressureToSeaLevel,
 	toKelvinFromCelcius,
+	getPressureClosestTo,
 	MINUTES,
 	KELVIN
 }
