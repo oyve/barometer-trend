800c56243c88694e462787e055fd1e7404592c98
Oyvind Hansen <oyvind.hansen@outlook.com>
2021-07-04 16:43:59 +0000

    use regression to detect front tendency

--- predictions/front.js
+++ predictions/front.js
@@ -1,5 +1,8 @@
 const fronts = require('./fronts.json');
 const utils = require('../utils');
+const regression = require('regression');
+
+const Pascal10 = 10;
 
 /**
  * 
@@ -7,33 +10,51 @@ const utils = require('../utils');
  * @returns {Array<Object>} Front JSON object
  */
 function getFront(pressures) {
-	let threeHourPressure = utils.getPressuresSince(pressures, -180)[0];
-	let twoHourPressure = utils.getPressuresSince(pressures, -120)[0];
-	let oneHourPressure = utils.getPressuresSince(pressures, -60)[0];
-	let nowPressure = pressures[pressures.length - 1];
+	let threeHourPressures = utils.getPressuresByPeriod(pressures, utils.minutesFromNow(-180), utils.minutesFromNow(-120));
+	let twoHourPressures = utils.getPressuresByPeriod(pressures, utils.minutesFromNow(-120), utils.minutesFromNow(-60));
+	let oneHourPressures = utils.getPressuresByPeriod(pressures, utils.minutesFromNow(-60), new Date());
 
-	return analyzePressures(threeHourPressure.value, twoHourPressure.value, oneHourPressure.value, nowPressure.value);
+	return analyzePressures(threeHourPressures, twoHourPressures, oneHourPressures);
 }
 
-function analyzePressures(hourThreePressure, hourTwoPressure, hourOnePressure, nowPressure)
-{
-	let t1 = getTendency(hourThreePressure, hourTwoPressure);
-	let t2 = getTendency(hourTwoPressure, hourOnePressure);
-	let t3 = getTendency(hourOnePressure, nowPressure);
+function analyzePressures(hourThreePressures, hourTwoPressures, hourOnePressures) {
+	let frontNull = fronts.find((f) => f.key === null);
+
+	if (!(hourThreePressures && hourTwoPressures && hourOnePressures)) return frontNull;
+
+	let t1 = getTendency(hourThreePressures, 180);
+	let t2 = getTendency(hourTwoPressures, 120);
+	let t3 = getTendency(hourOnePressures, 60);
+
+	if(!(t1 && t2 && t3)) return frontNull;
 
 	let key = t1.concat(t2, t3);
 	let front = fronts.find((f) => f.key === key);
-	return front !== undefined ? front : fronts.find((f) => f.key === "N/A");
+	return front !== undefined ? front : frontNull;
 }
 
-const Pascal10 = 10;
+function regressPressures(pressures) {
+	let minutelyPressures = [];
+	let i = 0;
+	let now = new Date();
+	 
+	pressures.forEach((p) => {
+		let diff = now - p.datetime;
+		let min = Math.round((diff/1000)/60);
+		minutelyPressures.push([min, p.value]);
+	});
 
-function getTendency(earlier, later) {
-	let difference = earlier - later;
-	
-	if(Math.abs(difference) < Pascal10) return "S"; //STEADY
-	if(difference > 0) return "F"; //FALLING
-	if(difference < 0) return "R"; //RISING
+	let result = regression.linear(minutelyPressures);
+	return result;
+}
+
+function getTendency(pressures, start) {
+	let regression = regressPressures(pressures);
+
+	let difference = regression.predict(start)[1] - regression.predict(start + 60)[1];
+	if (Math.abs(difference) < Pascal10) return "S"; //STEADY
+	if (difference > 0) return "R"; //RISING
+	if (difference < 0) return "F"; //FALLING
 }
 
 module.exports = {
