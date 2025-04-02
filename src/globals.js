cb57dd2b2793838a6064abcded56a6ef0c269f50
oyve <oyve-github@outlook.com>
2025-04-02 10:13:04 +0200

    keepPressureReadingsFor

--- src/globals.js
+++ src/globals.js
@@ -1,17 +1,25 @@
+const defaults = {
+    meanSeaLevelTemperature: 15,
+    isDiurnalEnabled: false,
+    keepPressureReadingsFor: 60*48 //48 hours
+}
+
+
 /**
  * Global constants
  */
 class Globals {
     constructor() {
-        this.meanSeaLevelTemperature = 15; //celcius
-        this.isDiurnalEnabled = false;
+        this.meanSeaLevelTemperature = defaults.meanSeaLevelTemperature; //celcius
+        this.isDiurnalEnabled = defaults.isDiurnalEnabled;
+        this.keepPressureReadingsFor = defaults.keepPressureReadingsFor; //48 hours
     }
 
     /**
      * 
      * @param {number} value Mean temperature at sea level 
      */
-    setMeanSeaLevelTemperature(value) {
+    setMeanSeaLevelTemperature(value = defaults.meanSeaLevelTemperature) {
         this.meanSeaLevelTemperature = value;
     }
 
@@ -19,8 +27,16 @@ class Globals {
      * 
      * @param {boolean} value True or false
      */
-    setIsDiurnalEnabled(value = false) {
+    setIsDiurnalEnabled(value = defaults.isDiurnalEnabled) {
         this.isDiurnalEnabled = value;
+    }
+
+    /**
+     * 
+     * @param {number} value Number of whole minutes to keep pressure readings for. Default: 48 hours.
+     */
+    setKeepPressureReadingsFor(value = defaults.keepPressureReadingsFor) {
+        if(Number.isInteger(value) && value > 0) this.keepPressureReadingsFor = value;
     }
 }
 
