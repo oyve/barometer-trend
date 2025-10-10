19b4a19a31f2cb1ca4063ca2caff28b9d59f67cc
Oyve <oyve-github@outlook.com>
2025-10-10 09:51:47 +0200

    remove folder

--- scripts/generate-exports.cjs
+++ scripts/generate-exports.cjs
@@ -1,13 +1,8 @@
+const { dir } = require('console');
 const fs = require('fs');
 const path = require('path');
 
 function generateExports(directory) {
-  // Define paths
-  const cjsDir = path.resolve(__dirname, `../dist/cjs/${directory}`);
-  const esmDir = path.resolve(__dirname, `../dist/esm/${directory}`);
-
-  // Get all JavaScript files in the ESM directory
-  const files = fs.readdirSync(esmDir).filter(file => file.endsWith('.js'));
 
   // Initialize the exports field with the default index file
   const exportsField = {
@@ -18,23 +13,33 @@ function generateExports(directory) {
     },
   };
 
-  // Add entries for other files dynamically
-  files.forEach(file => {
-    const name = `./${path.basename(file, '.js')}`;
-    exportsField[name] = {
-      require: `./dist/cjs/${directory}/${file.replace('.js', '.cjs')}`,
-      import: `./dist/esm/${directory}/${file}`,
-      types: `./dist/types/${directory}/${path.basename(file, '.js')}.d.ts`
-    };
-  });
+  if(directory !== null) {
+      // Define paths
+    const cjsDir = path.resolve(__dirname, `../dist/cjs/${directory}`);
+    const esmDir = path.resolve(__dirname, `../dist/esm/${directory}`);
 
-  // Read, update, and write the package.json file
+    // Get all JavaScript files in the ESM directory
+    const files = fs.readdirSync(esmDir).filter(file => file.endsWith('.js'));
 
+
+    // Add entries for other files dynamically
+    files.forEach(file => {
+      const name = `./${path.basename(file, '.js')}`;
+      exportsField[name] = {
+        require: `./dist/cjs/${directory}/${file.replace('.js', '.cjs')}`,
+        import: `./dist/esm/${directory}/${file}`,
+        types: `./dist/types/${directory}/${path.basename(file, '.js')}.d.ts`
+      };
+    });
+  }
+
+  // Read, update, and write the package.json fil
   console.log(`Generated exports for directory /${directory}`);
   return exportsField;
 }
 
-let predictions = generateExports('predictions');
+//let predictions = generateExports('predictions');
+let predictions = generateExports(null);
 
 const exportsField = {...predictions };
 
